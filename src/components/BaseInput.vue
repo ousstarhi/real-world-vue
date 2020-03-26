c1f9f0a430da9a4b7c0eeacb57b5e3e623ee3e85
ousstarhi <ousstarhi.web@gmail.com>
2020-03-26 17:14:03 +0100

    creating BaseInput component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <form>
+      <input type="text" :placeholder="placeholder" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
